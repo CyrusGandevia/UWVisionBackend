db03a79dde6d2d43efc34dacd2aa697802b584db
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2022-07-19 01:07:12 -0400

    tentatively finished company API

--- UWVision/company/api.py
+++ UWVision/company/api.py
@@ -3,13 +3,31 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Company
 from .serializers import CompanySerializer
-from django.core import serializers
+from django.db.models import Count
+from django.core.exceptions import ObjectDoesNotExist
 
 @api_view(['GET'])
-def get_all_companies(request):
-    companies = Company.objects.all()
-    serializer = CompanySerializer(companies, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+def get_searchbar_data(request):
+    # 1) Fetches all companies
+    # 2) Joins with all jobs 
+    # 3) Groups by company name and aggregates number of jobs per company
+    # 4) Returns list of {company_name, company_id, job_count} objects
+    companies = Company.objects.values('name', 'id').annotate(job_count=Count('job__name')).order_by('name')
+    return Response(data=list(companies), status=status.HTTP_200_OK)
+
+# TODO: Serialize data? Prevent SQL injection..?
+@api_view(['GET'])
+def get_company_data(request, **kwargs):
+    company_id = kwargs.get('company_id', None)
+
+    if not company_id:
+        return Response({'error': 'No company id passed!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        company = Company.objects.values().get(id=company_id)
+        return Response(data=company, status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        return Response({'error': 'Company with id=' + str(company_id) + ' does not exist.'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def create_company(request):
--- UWVision/company/urls.py
+++ UWVision/company/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .api import get_all_companies, create_company
+from .api import get_searchbar_data, get_company_data, create_company
 
 urlpatterns = [
-    path('get', get_all_companies),
-    path('create', create_company)
+    # GET requests:
+    path('get/searchbar_data', get_searchbar_data),
+    path('get/company/<int:company_id>', get_company_data),
+
+    # POST requests:
+    path('create/company', create_company)
 ]
