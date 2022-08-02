d696a63a1dbac0829958f94223b6a6dfca8904ab
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2022-08-01 22:05:05 -0400

    updated for ElasticBeanstalk

--- UWVision/settings.py
+++ UWVision/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +25,6 @@ SECRET_KEY = 'django-insecure-v_-6gliqst5ir*y@rgzb1(brqs-5yly@n1q@owi7e*u0cq%vjp
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ["UWVision-test.us-east-1.elasticbeanstalk.com"]
 
 
 # Application definition
@@ -97,15 +96,28 @@ WSGI_APPLICATION = 'UWVision.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cyrusgandevia',
-        'USER': 'cyrusgandevia',
-        'PASSWORD': '',
-        'HOST': 'localhost'
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'cyrusgandevia',
+            'USER': 'cyrusgandevia',
+            'PASSWORD': '',
+            'HOST': 'localhost'
+        }
+    }
 
 
 # Password validation
